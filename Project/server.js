1518946ebdbeb0528ebabe90739ecedb9949a703
Xing Du <xingdu@Xings-MacBook-Air.local>
2018-02-26 18:19:58 -0800

    tab fix needed

---
+++ Project/server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+//app.listen(process.env.PORT || 5000);
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+  console.log("Listening on Port 3000");
+});

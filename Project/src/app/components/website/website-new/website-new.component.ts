cfee6c7e7e1ede7eda243750e2d3b1a96cd909ec
vickyzhang <zhangqivicky@zhangqis-MacBook-Pro.local>
2018-04-04 23:37:24 -0700

    another changeto newevent

--- Project/src/app/components/website/website-new/website-new.component.ts
+++ Project/src/app/components/website/website-new/website-new.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../../services/user.service.client';
+import {WebsiteService} from '../../../services/website.service.client';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-website-new',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./website-new.component.css']
 })
 export class WebsiteNewComponent implements OnInit {
-
-  constructor() { }
+  userId: string;
+  constructor(private userService: UserService,
+              private websiteService: WebsiteService,
+              private activeRouter: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    this.activeRouter.params.subscribe(params => {
+      this.userId = params['uid'];
+  });
   }
-
+  toProfile() {
+    this.router.navigate(['/user/' + this.userId]);
+  }
 }

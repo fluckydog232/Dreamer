7f68ee1b138a4095752c0b4913fd4dcc0e786d0a
vickyzhang <zhangqivicky@zhangqis-MacBook-Pro.local>
2018-04-04 19:18:32 -0700

    small change margin-top

--- Project/src/app/components/website/website-list/website-list.component.ts
+++ Project/src/app/components/website/website-list/website-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {WebsiteService} from '../../../services/website.service.client';
+import {User} from '../../../models/user.model.client';
+import {Website} from '../../../models/website.model.client';
+import {UserService} from '../../../services/user.service.client';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-website-list',
@@ -7,11 +13,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebsiteListComponent implements OnInit {
 
-  constructor() { }
+  userId: String;
+  websiteName: String;
+  website: Website;
+  savedwebsites: Website[] = [];
+  goingwebsites: Website[] = [];
+  user: User;
+
+  constructor(private userService: UserService,
+              private websiteService: WebsiteService,
+              private router: ActivatedRoute) { }
 
   ngOnInit() {
+    this.router.params.subscribe(params => {
+      this.userId = params['uid'];
+      this.user = this.userService.findUserById(this.userId);
+      console.log('userId: ', this.user._id);
+      this.websiteService.findSavedEventsByUser(this.userId)
+        .subscribe((savedwebsites) => {
+          console.log(savedwebsites);
+          this.savedwebsites = this.savedwebsites.concat(savedwebsites);
+        });
+      this.websiteService.findGoingEventsByUser(this.userId).subscribe(goingwebsites => {
+        console.log(goingwebsites);
+        // this.goingwebsites = goingwebsites;
+      });
+      // this.savedwebsites = this.websiteService.findSavedEventsByUser(this.userId);
+      // this.goingwebsites = this.websiteService.findGoingEventsByUser(this.userId);
+    });
   }
-  toProfile() {
 
-  }
 }

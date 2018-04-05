23264201ef37987e0ec16714c9c7afc30352cb53
vickyzhang <zhangqivicky@zhangqis-MacBook-Pro.local>
2018-04-04 21:48:23 -0700

    change login

--- Project/src/app/components/user/login/login.component.ts
+++ Project/src/app/components/user/login/login.component.ts
@@ -12,18 +12,23 @@ export class LoginComponent implements OnInit {
 
   email: string;
   password: string;
+  errorFlag: boolean;
+  errorMsg: string;
 
   constructor(private userService: UserService,
               private router: Router) {}
+  ngOnInit() {
+  }
 
   login() {
     const user: User = this.userService.findUserByCredentials(this.email, this.password);
     if (user) {
+      this.errorFlag = false;
       this.router.navigate(['/user', user._id]);
+    } else {
+      this.errorFlag = true;
+      this.errorMsg = 'Invalid email or password !';
     }
-  }
-
-  ngOnInit() {
   }
 
 }
